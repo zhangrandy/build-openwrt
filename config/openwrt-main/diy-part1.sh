edd5d582330b190e8c4f862aa654db192d9a42b9
zhangrandy <zhangrandy@gmail.com>
2025-06-20 15:09:07 +0800

    Update diy-part1.sh

--- config/openwrt-main/diy-part1.sh
+++ config/openwrt-main/diy-part1.sh
@@ -8,6 +8,8 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git passwall passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
 
 # other
 # rm -rf package/utils/{ucode,fbtest}
